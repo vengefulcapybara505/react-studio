88946b77bceb24e39b3cd1911d6e9df969249a19
justinHic <justinhickey22@yahoo.com>
2022-11-09 21:49:06 -0500

    yuck

--- src/App.js
+++ src/App.js
@@ -12,20 +12,77 @@ function App() {
   // TODO: use useState to create a state variable to hold the state of the cart
   /* add your cart state code here */
 
+  const [cart, setCart] = useState({});
+  const [price, setPrice] = useState(0);
+  const updateCart =(uid)=>
+  {
+    console.log(uid);
+    let newCart = cart;
+    if(newCart[uid])
+    {
+      newCart[uid] +=1;
+    }
+    else{
+      newCart[uid] = 1;
+    }
+
+    setCart({...newCart})
+
+    setPrice(price + bakeryData[uid].price)
+    console.log(cart);
+    console.log(bakeryData[0].name)
+  }
+
   return (
     <div className="App">
       <h1>My Bakery</h1> {/* TODO: personalize your bakery (if you want) */}
 
       {bakeryData.map((item, index) => ( // TODO: map bakeryData to BakeryItem components
-        <p>Bakery Item {index}</p> // replace with BakeryItem component
+        <div>
+          <h2>{item.name}</h2>
+          <img style = {{width: 50 + '%'}} src = {item.image} />
+          <p>{item.price}</p>
+          <CartItems updateCart = {updateCart} item = {item} index = {index}/>
+          </div>
+         // replace with BakeryItem component
       ))}
 
       <div>
-        <h2>Cart</h2>
-        {/* TODO: render a list of items in the cart */}
+        <h2>Cart {price}</h2>
+        {/* TODO: render a list of items in the cart */
+
+        Object.keys(cart).map((key) => {
+          return (
+            <div>
+          <h2>
+            {bakeryData[key].name}
+          </h2>
+          <h2>
+            {cart[key]}
+          </h2>
+          </div>
+          )
+          
+              })
+        /*cart.map((index, item) =>
+        (
+          <div>
+            <h2>
+              
+            </h2>
+            <h2>
+            
+            </h2>
+            </div>
+        ))*/}
       </div>
     </div>
   );
+}
+
+function CartItems(props)
+{
+  return (<button onClick = {() => props.updateCart(props.index)}> test </button>)
 }
 
 export default App;
